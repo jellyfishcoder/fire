111d9643a34f16875c0d8b8f16b7e6c01fa67e92
Jellyfishcoder <jellyfishcoder@gmail.com>
2016-05-18 19:30:36 -0500

    Added gemfiles and initial code

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'url', '~> 0.3.2'
+gem 'download', '~> 1.1'
+# gem 'rake', '~> 11.1', '>= 11.1.2'
+# gem "rails"
---
+++ fire.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/env ruby
+# fire.rb
+
+# Gems
+require 'optparse'	# for parsing arguments
+require 'socket'	# for web stuff
+require 'ipaddr'	# for IP addresses
+require 'download'	# for downloading (will replace with multiconnect code later)
+require 'url'
+
+=begin
+ URL Handler Class
+ Can: get IP from domain name
+ Can do in future: Check if server is responding
+=end
+class urlhandler
+	attr_accessor :url
+	# Allows you to set url at startup
+	def initialize(url)
+		@full_url=url
+	end
+	# Function to get base domain from URL
+	def urltodomain
+		puts "Extracting domain name from URL to do DNS lookup"
+		domain = @full_url .split
+	# Function to look up IP from Domain
+	def dnslookup
+		puts "Looking up IP of domain from DNS server..."
+		ipaddr = IPAddr.getaddress "#@domain"
+	
+# Setup OptionParser for arguments passed to command
+options = {}
+
+fire_parse = OptionParser.new do |opt|
+	opt.banner = "Usage: fire URL [OPTIONS]
+	opt.separator " "
+	opt.separator "URL"
+	opt.separator "	Replace URL with the URL of the file you would like to download"
+	# Optional arguments for command
+	opt.separator "Options"
+
+	# Route trafic over TOR
+	#opt.on("-t","--tor","route trafic through TOR network \not implemented/) do
+	#	options[:tor] = true
+	#end
+
+	# Run in background
+	opt.on("-b","--background","run in background and do not display progress") do
+		options[:background] = true
+	end
+
+	# Show help page
+	opt.on("-h","--help","display help page") do
+		puts fire_parse
+	end
+end
+
+=begin
+ Method to extract options from ARGV
+ As each argument (starting from back) is parsed,
+ it is deleted. This means they must be saved by
+ code in the options array when encountered.
+=end
+fire_parse.parse!
+
+ifARGV[0] 
+# If they neglect to provide ARGV, print the help
+when ""
+	puts fire_parse
+else
+	
